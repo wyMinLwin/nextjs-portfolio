86a1c1df5056bde68e91ad10a29f43c52b805a46
wyMinLwin <waiyanminlwin421@gmail.com>
2023-12-11 21:06:24 +0630

    :zap: Image Optimization

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -10,11 +10,19 @@ const karla = Karla({ subsets: ['latin'] })
 
 let projects = [
   {
+    title: "GuideHub",
+    desc: "GuideHub is a personal productivity management tool. Some features are still in process.",
+    demo: "https://guide-hub.vercel.app/",
+    github: "https://github.com/wyMinLwin/GuideHub",
+    image: 'GuideHub.webp',
+    tools: ["Next","TanStackQuery","Framer Motion","Redux Toolkit","Tailwind"]
+  },
+  {
       title: "React POS",
       desc: "This is not a traditional pos with a bunch of table layout. I used clean component card layout ui. I implemented TanStack Query to that app with Supabase. I still maintaining this app as long as with my SvelteKit Friends app.",
       demo: "https://wyml-react-pos.vercel.app",
       github: "https://github.com/wyMinLwin/react-pos",
-      image: 'ReactPos.png',
+      image: 'ReactPos.webp',
       tools: ["React","TanStackQuery","Supabase","Redux Toolkit","Tailwind"]
   },
   {
@@ -22,7 +30,7 @@ let projects = [
       desc: "NextJS 13 SSG with framer motion. The project idea was from <a class='underline' href='/https://github.com/sannlynnhtun-coding/Zodiac/'>this repo<a/> and also I was willing to implement framer motion. So this is the result. ",
       demo: "https://zodiac-next.vercel.app/",
       github: "https://github.com/wyMinLwin/zodiac-next",
-      image: 'NextZodiac.png',
+      image: 'NextZodiac.webp',
       tools: ["Next","Framer Motion","PNPM","TypeScript","Tailwind"]
   },
   {
@@ -30,7 +38,7 @@ let projects = [
       desc: "This is my SvelteKit app that I still maintaining. My goal about this project is to give seamless user experiences to users. I mainly focused on user interaction. The purpose of app is like tinder with in case of friends. Me and one of my coder friends about to collaborate in this project.",
       demo: "https://svelte-friends.vercel.app/login",
       github: "https://github.com/wyMinLwin/svelte-friends",
-      image: 'SvelteFriend.png',
+      image: 'SvelteFriend.webp',
       tools: ["SvelteKit","TypeScript","Tailwind","PNPM"]
   },
   {
@@ -38,7 +46,7 @@ let projects = [
     desc: "",
     demo: "https://waiyanminlwin-v3.vercel.app",
     github: "https://github.com/wyMinLwin/svelte-portfolio",
-    image: 'V3.png',
+    image: 'V3.webp',
     tools: ["SvelteKit","TypeScript","Tailwind","Vite"]
 },
   {
@@ -46,7 +54,7 @@ let projects = [
       desc: "The idea of developing this app is really personal cause I really like to use terminal and CLI. I build the project with Vite As a first I started working on layout to get a clean layout. Since there was one page It was easy to create a layout. I used Redux Toolkit to store the history of command and write a bunch of straight forward JavaScirpt method to give a response to user. The major problem I faced with this projects are getting the history with the key storke and try to display that on the screen and think about how should the command typing work without using the HTML built-in Input tag.",
       demo: "https://wyminlwin-terminal-ts.netlify.app/",
       github: "https://github.com/wyMinLwin/terminal-react-ts",
-      image: 'ReactTerminal.png',
+      image: 'ReactTerminal.webp',
       tools: ["React","TypeScript","Tailwind","Redux Toolkit","Vite"]
   },
   {
@@ -54,7 +62,7 @@ let projects = [
       desc: "As long as I remember this is my first React project with my own idea without any tutorial base. I used Bootstrap for theme. I used a JavaScript library to generate words. The most difficult problems were rendering out reactively which words are wrong or right. I was also about to add a virtual keyboard which would what keys you are typing. But due to render issues, I decide not to add cause I didn't know so much at those time.",
       demo: "https://wyminlwin-type.netlify.app/",
       github: "https://github.com/wyMinLwin/typing-speed",
-      image: 'TypingSpeed.png',
+      image: 'TypingSpeed.webp',
       tools: ["React","JavaScript","Bootstrap"]
   },
   {
@@ -62,7 +70,7 @@ let projects = [
       desc: "I created this web to train React. The idea was orginally came from a video about managing state in React and I started create this. I used React, Bootstrap, Sass a little bit and React router dom.",
       demo: "https://wyminlwin-dropnote.netlify.app/",
       github: "https://github.com/wyMinLwin/dropnote/",
-      image: 'NoteApp.png',
+      image: 'NoteApp.webp',
       tools: ["React","Bootstrap","Sass",]
   },
   {
@@ -70,7 +78,7 @@ let projects = [
       desc: "After I focused on React, I decided to write a portfolio with React. I didn't a bunch of research and create this portfolio.",
       demo: "https://wyminlwin.netlify.app/",
       github: "https://github.com/wyMinLwin/react-portfolio",
-      image: 'Portfolio.png',
+      image: 'Portfolio.webp',
       tools: ["React","Bootstrap"]
   },
   
