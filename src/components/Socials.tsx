6eb683082585c690bd4197e6d4719c9b03be1b73
wyMinLwin <waiyanminlwin421@gmail.com>
2024-02-22 11:34:04 +0630

    :bug: Social Links

--- src/components/Socials.tsx
+++ src/components/Socials.tsx
@@ -1,34 +1,40 @@
-import Image from 'next/image'
-import React from 'react'
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 const socials = [
     {
-        img: 'github.svg',
-        url: 'https://github.com/wyMinLwin'
+        img: "github.svg",
+        url: "https://github.com/wyMinLwin",
     },
     {
-        img: 'instagram.svg',
-        url: 'https://www.instagram.com/inverted_niwl/'
+        img: "instagram.svg",
+        url: "https://www.instagram.com/neil_the_butterfly/",
     },
     {
-        img: 'linkedin.svg',
-        url: 'https://www.linkedin.com/in/wyMinLwin'
+        img: "linkedin.svg",
+        url: "https://www.linkedin.com/in/wyMinLwin",
     },
     {
-        img: 'facebook.svg',
-        url: 'https://www.facebook.com/waiyan.minlwin.161/'
-    }
-]
+        img: "facebook.svg",
+        url: "https://www.facebook.com/waiyan.minlwin.161/",
+    },
+];
 const Socials = () => {
+    return (
+        <div className="flex items-center gap-x-10 py-2">
+            {socials.map((social) => (
+                <Link href={social.url} key={social.url}>
+                    <Image
+                        priority
+                        src={"/images/" + social.img}
+                        alt={social.url}
+                        width={20}
+                        height={20}
+                    />
+                </Link>
+            ))}
+        </div>
+    );
+};
 
-  return (
-    <div className='flex gap-x-10 py-2 items-center'>
-        {
-            socials.map(social => (
-                <Image key={social.url} priority src={'/images/'+social.img} alt={social.url} width={20} height={20} />
-            ))
-        }
-    </div>
-  )
-}
-
-export default Socials
+export default Socials;
